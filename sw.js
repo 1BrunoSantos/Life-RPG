12a892466c2891c6905f63c1007909de80a8fcce
Bruno Santos <142893092+1BrunoSantos@users.noreply.github.com>
2026-01-14 17:39:08 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,45 @@
-const CACHE_NAME = "rpg-bruno-v1";
-const ASSETS = ["./", "./index.html", "./style.css", "./game.js", "./icon.png", "./manifest.json"];
+// Mudei o nome aqui para "v2" para forçar a atualização
+const CACHE_NAME = "liferpg-bruno-v2"; 
 
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./icon.png",
+  "./manifest.json"
+];
+
+// Instalação
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting(); // Força a atualização imediata
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Ativação (Limpa caches antigos)
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Requisição
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
